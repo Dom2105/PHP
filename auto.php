f50ea6e632a9b77d98943155432f224036c5fe75
Dom2105 <55178791+Dom2105@users.noreply.github.com>
2020-08-19 15:50:08 +0200

    update

--- auto.php
+++ auto.php
@@ -9,7 +9,7 @@ echo date('d.m.y \u\m H:i:s');
 
 <?php 
 #String
-$string = 'hallo'; #Zeichenkette 
+$string = date('d.m.y'); #Zeichenkette 
 #Integer
 $zahl = 12; #Ganzezahl  -9223372036854775808 bis 9223372036854775807 64bit (-2147483647 bis 2147483647 32bit)
 #float
@@ -81,17 +81,74 @@ foreach ($userID as $user) {
 <?php
 $userDaten = [
     #0
-    ["Peter","Wild",1],
+    [1,"Peter","Wild",5,"w"],
     #1
-    [2,"Hans","Gans"],
+    [2,"Hans","12",5,"w"],
     #2
-    [3,"Franz","Lustig"]
+    [3,"Franz","Lustig",5,"w"],
+    [2,2]
 ];
 
 
-$userDaten[1][] = 2;
-echo $userDaten[1][3];
+#$userDaten[1][] = $string;
+echo $userDaten[1][3].'<br>';
+
+for ($i=0; $i < count($userDaten); $i++) { 
+    echo '<br>';
+    for ($j=0; $j < count(end($userDaten)) ; $j++) { 
+        echo $userDaten[$i][$j].' ';
+    }
+}
+
+/* foreach ($userDaten as $key => $value) {
+    echo $key.' '.$value;
+} */
+
+?><br><hr><br>
+
+<?php
+$zahl2 = 5;
+
+$türeAufZu = true;
+
+switch (!$zahl) {
+    case 11:
+        echo "Zahl ist 11";
+        break;
+    case 10:
+        echo "Zahl ist 10";
+        break;
+    case 12:
+        echo"Zahl ist 11";
+    break;
+    
+    default:
+        echo "Die Zahl ist nicht vorhanden!";
+        break;
+}
+echo '<br><hr><br>';
+
+#Logische Operatoren UND = && and ; ODER= || or ;
+#Exklusivesoder = xor ; Negierung = ! zb. !$zahl ;
+if (($zahl != 5) || ($zahl2 == 5)) {
+    echo "bedingung erfüllt";
+}else {
+    echo "else ist nicht richtig";
+}
+
+echo '<br><hr><br>';
+
+if (!$türeAufZu == true) {
+    echo "Türe ist auf";
+}else{
+    echo "Tür ist zu";
+}
 ?>
+
+<?php 
+include('./person.php');
+?>
+
 
 
 
